7ae0df7ebe1347e3d9af539ab742ce49775603e3
Christopher Helgeson <ChristopherHelgeson@hotmail.com>
2017-09-22 09:58:54 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WeatherUndergroundAPI.Models;
+
+namespace WeatherUndergroundAPI.Controllers
+{
+    // Weather Underground API key: 12b8a40c145b360a
+    // Sample: http://api.wunderground.com/api/12b8a40c145b360a/conditions/q/CA/San_Francisco.json
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Weather(string zip)
+        {
+            //int zipcode = 45243;
+
+            WeatherData weatherJSON = new WeatherData();
+
+            JObject x = weatherJSON.CreateObject(zip);
+
+            string[] locationInfo = weatherJSON.GetLocationInfo(x);
+            ViewBag.LocationInfo = locationInfo;
+
+            string[] weatherInfo = weatherJSON.GetLocationWeather(x);
+            ViewBag.WeatherInfo = weatherInfo;
+
+            return View();
+        }
+    }
+}
---
+++ Models/WeatherData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+using System.ComponentModel.DataAnnotations;
+
+namespace WeatherUndergroundAPI.Models
+{
+    public class WeatherData
+    {
+        string URLheader = "http://api.wunderground.com/api/";
+        string APIkey = "12b8a40c145b360a";
+        string param = "/conditions/q/";
+        string extension = ".json";
+
+        public JObject CreateObject(string i)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(URLheader + APIkey + param + i + extension);
+            request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string ApiText = rd.ReadToEnd();
+            JObject o = JObject.Parse(ApiText);
+            return o;
+        }
+
+        public string[] GetLocationInfo(JObject o)
+        {
+            string[] locationInfo = new string[]
+            {
+                (o["current_observation"]["display_location"]["full"]).ToString(),
+                (o["current_observation"]["observation_location"]["latitude"]).ToString(),
+                (o["current_observation"]["observation_location"]["longitude"]).ToString(),
+                (o["current_observation"]["observation_location"]["elevation"]).ToString(),
+                (o["current_observation"]["observation_time"]).ToString()
+            };
+            return locationInfo;
+        }
+
+        public string[] GetLocationWeather(JObject o)
+        {
+            string[] locationWeather = new string[]
+            {
+                (o["current_observation"]["weather"]).ToString(),
+                (o["current_observation"]["temperature_string"]).ToString(),
+                (o["current_observation"]["dewpoint_string"]).ToString(),
+                (o["current_observation"]["relative_humidity"]).ToString(),
+                (o["current_observation"]["wind_dir"] + " (" + o["current_observation"]["wind_degrees"] + " degrees) @ " + o["current_observation"]["wind_mph"] + " mph").ToString(),
+                (o["current_observation"]["pressure_in"] + "\"").ToString(),
+                (o["current_observation"]["icon_url"]).ToString()
+             };
+            return locationWeather;
+        }
+    }
+
+    public class Input
+    {
+        [Required]
+        public string Zipcode { get; set; }
+    }
+}
